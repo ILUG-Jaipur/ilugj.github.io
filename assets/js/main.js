c227202623931d269626cc743dfa256b0721b6b7
Rishi Banerjee <rishieric91@gmail.com>
2019-05-21 16:07:11 +0530

    Initial Commit

---
+++ assets/js/main.js
@@ -0,0 +1,31 @@
+function display_next_event(el) {
+
+    var captions = ["Let's meetup at: ", "Next event:", "Upcoming:", "Next up: "]
+    var caption = captions[Math.floor(Math.random()*captions.length)],
+        meetup_group = $(el).attr('data-meetup-group');
+    var timeConverter = function(UNIX_timestamp){
+      var a = new Date(UNIX_timestamp);
+      var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+      var year = a.getFullYear();
+      var month = months[a.getMonth()];
+      var date = a.getDate();
+      var time = date + ' ' + month + ' ' + year;
+      return time;
+    }
+    console.log('https://api.meetup.com/'+meetup_group+'?photo-host=public&sig_id=230922245&only=next_event&sig=fc0267bcfbe5a31e01f52138451f0db0477a7412');
+    $.ajax({
+      url: 'https://api.meetup.com/'+meetup_group+'?photo-host=public&sig_id=230922245&only=next_event&sig=fc0267bcfbe5a31e01f52138451f0db0477a7412',
+      jsonp: 'callback',
+      dataType: 'jsonp',
+      success: function( response ) {
+        console.log(response)
+          var next_event = response.data.next_event;
+          if (next_event){
+            var event_title = next_event.name + ' (' + timeConverter(next_event.time) + ')';
+            var event_url = 'http://www.meetup.com/'+meetup_group+'/events/' + next_event.id;
+            var html = '<span class=caption>'+ caption +'</span><a target="_blank" href=' + event_url + '>' + event_title + '</a>';
+            $(el).html(html).css({'opacity': 1});
+          };
+      }
+    });
+}
